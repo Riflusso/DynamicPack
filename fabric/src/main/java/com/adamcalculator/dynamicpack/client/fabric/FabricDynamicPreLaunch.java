71dbbced0f95e116b5078d87eaa7534f33c1cdd4
adam <adamcalculator272@gmail.com>
2024-07-18 16:50:11 +0300

    - libjf added as dependency - added build git hash to fabric launch

--- fabric/src/main/java/com/adamcalculator/dynamicpack/client/fabric/FabricDynamicPreLaunch.java
+++ fabric/src/main/java/com/adamcalculator/dynamicpack/client/fabric/FabricDynamicPreLaunch.java
@@ -1,10 +1,12 @@
 package com.adamcalculator.dynamicpack.client.fabric;
 
+import com.adamcalculator.dynamicpack.SharedConstrains;
 import com.adamcalculator.dynamicpack.client.DynamicPackModBase;
 import com.adamcalculator.dynamicpack.util.Loader;
 import com.adamcalculator.dynamicpack.util.Out;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.loader.api.metadata.CustomValue;
 
 /**
  * Fabric impl for DynamicPack mod
@@ -16,6 +18,13 @@ public class FabricDynamicPreLaunch extends DynamicPackModBase implements PreLau
     public void onPreLaunch() {
         instance = this;
         Out.println("DynamicPack loaded. Hello fabric world!");
+
+        try {
+            CustomValue customValue = FabricLoader.getInstance().getModContainer(SharedConstrains.MOD_ID).get().getMetadata().getCustomValue("dynamicpack:build_git_hash");
+            String asString = customValue.getAsString();
+            Out.println("Build git hash: " + asString);
+
+        } catch (Exception ignored) {}
 
         var gameDir = FabricLoader.getInstance().getGameDir().toFile();
         init(gameDir, Loader.FABRIC);
